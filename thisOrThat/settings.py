f561ac7c0f39b769fc2f91cdfb505bb1e9f0206b
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-06-12 11:44:41 +0100

    database settings configured for PSQL database

--- thisOrThat/settings.py
+++ thisOrThat/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from environment_manager import Manager
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +76,15 @@ WSGI_APPLICATION = 'thisOrThat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+database_credentials = Manager().get_database_credentials()
+database_credentials.update(
+    {'ENGINE': 'django.db.backends.postgresql_psycopg2'}
+)
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
+    'default': database_credentials
+    
 }
 
 
@@ -117,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
