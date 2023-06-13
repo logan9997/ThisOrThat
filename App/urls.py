03e6dd1d31f4bf1227dfe313d7b2df93b0c7b34d
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-06-13 23:25:02 +0100

    newly created views added to urlpatterns

--- App/urls.py
+++ App/urls.py
@@ -1,10 +1,32 @@
 from django.urls import path
 from .views import (
-    home
+    home, post, login, create,
+    search, signup, logout, vote
 )
 
 urlpatterns = [
     path(
         '', home.home, name='home'
-    )
+    ),
+    path(
+        'post/<int:post_id>/', post.post, name='post'
+    ),
+    path(
+        'login/', login.login, name='login'
+    ),
+    path(
+        'signup/', signup.signup, name='signup'
+    ),
+    path(
+        'logout/', logout.logout, name='logout'
+    ),
+    path(
+        'create/', create.create, name='create'
+    ),
+    path(
+        'search/', search.search, name='search'
+    ),
+    path(
+        'vote/<int:post_id>/', vote.vote, name='vote'
+    ),
 ]
