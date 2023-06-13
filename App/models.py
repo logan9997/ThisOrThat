cd4a5224535968d3c61769c2cb5f9de318358c24
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-06-13 11:24:25 +0100

    removed email field

--- App/models.py
+++ App/models.py
@@ -1,38 +1,37 @@
 from django.db import models
 from .config import (
-    USERNAME_LENGTH, EMAIL_LENGTH, PASSWORD_LENGTH,
-    TITLE_LENGTH, DESCRIPTION_LENGTH, STATUS_LENGTH,
-    TAGS_LENGTH, OPTION_LENGTH
+    MAX_USERNAME_LENGTH, MAX_EMAIL_LENGTH, MAX_PASSWORD_LENGTH,
+    MAX_TITLE_LENGTH, MAX_DESCRIPTION_LENGTH, MAX_STATUS_LENGTH,
+    MAX_TAGS_LENGTH, MAX_OPTION_LENGTH
 )
 
 
 class User(models.Model):
     user_id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=USERNAME_LENGTH)
-    email = models.EmailField(max_length=EMAIL_LENGTH)
-    password = models.CharField(max_length=PASSWORD_LENGTH)
+    username = models.CharField(max_length=MAX_USERNAME_LENGTH)
+    password = models.CharField(max_length=MAX_PASSWORD_LENGTH)
 
 
 class Post(models.Model):
     post_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=TITLE_LENGTH)
-    description_one = models.CharField(max_length=DESCRIPTION_LENGTH)
-    description_two = models.CharField(max_length=DESCRIPTION_LENGTH)
+    title = models.CharField(max_length=MAX_TITLE_LENGTH)
+    description_one = models.CharField(max_length=MAX_DESCRIPTION_LENGTH)
+    description_two = models.CharField(max_length=MAX_DESCRIPTION_LENGTH)
     image_one = models.ImageField(null=True, blank=True, upload_to='images/')
     image_two = models.ImageField(null=True, blank=True, upload_to='images/')
-    status = models.CharField(max_length=STATUS_LENGTH, default='Open', choices=(
+    status = models.CharField(max_length=MAX_STATUS_LENGTH, default='Open', choices=(
         ('Open', 'Open'), ('Closed', 'Closed')
     ))
     date_posted = models.DateField(auto_now_add=True)
-    tags = models.CharField(max_length=TAGS_LENGTH)
+    tags = models.CharField(max_length=MAX_TAGS_LENGTH)
 
 
 class Vote(models.Model):
     vote_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    option = models.CharField(max_length=OPTION_LENGTH, choices=(
+    option = models.CharField(max_length=MAX_OPTION_LENGTH, choices=(
         ('1', '1'), ('2', '2')
     ))
 
