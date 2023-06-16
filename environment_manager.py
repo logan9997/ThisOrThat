165599ae47e0cd8f417bd2e2e831279a24053934
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-06-16 18:39:31 +0100

    parameter in get_db_credentails for returning credentials for settings.py or psycopg2

--- environment_manager.py
+++ environment_manager.py
@@ -7,15 +7,25 @@ class Manager():
         self.env_path = '.env'
         load_dotenv(self.env_path)
 
-    def get_database_credentials(self):
-        credentials = {
-            'NAME': os.getenv('LOCAL_DB_NAME'),
-            'USER': os.getenv('LOCAL_DB_USER'),
-            'PASSWORD': os.getenv('LOCAL_DB_PASSWORD'),
-            'HOST': os.getenv('LOCAL_DB_HOST'),
-            'PORT': os.getenv('LOCAL_DB_PORT'),
-        }
+    def get_database_credentials(self, connection:str) -> dict:
+        if connection == 'settings':
+            credentials = {
+                'NAME': os.getenv('LOCAL_DB_NAME'),
+                'USER': os.getenv('LOCAL_DB_USER'),
+                'PASSWORD': os.getenv('LOCAL_DB_PASSWORD'),
+                'HOST': os.getenv('LOCAL_DB_HOST'),
+                'PORT': os.getenv('LOCAL_DB_PORT'),
+            }
+        elif connection == 'psycopg2':
+            credentials = {
+                'dbname': os.getenv('LOCAL_DB_NAME'),
+                'user': os.getenv('LOCAL_DB_USER'),
+                'password': os.getenv('LOCAL_DB_PASSWORD'),
+                'host': os.getenv('LOCAL_DB_HOST'),
+                'port': os.getenv('LOCAL_DB_PORT'),
+            }          
         return credentials
     
-    def get_development(self):
-        return os.getenv('DEVELOPMENT')
+    def get_values(self, *args) -> dict:
+        values = {arg : os.getenv(arg) for arg in args}
+        return values
