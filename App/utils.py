64bad7224b5bf0020bbec509b31e594683863d3c
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-06-16 18:30:42 +0100

    function for vote handling for comment votes and post votes

--- App/utils.py
+++ App/utils.py
@@ -1,8 +1,87 @@
 from password_strength import PasswordPolicy
+from django.db.models import Model
+from django.forms import Form
+from .models import Comment, Post
+from django.shortcuts import redirect
 from .config import (
     VALID_IMAGE_EXTENSIONS, MAX_USERNAME_LENGTH, MAX_PASSWORD_LENGTH,
-    MIN_PASSWORD_LENGTH, MIN_SPECIAL_CHARS, MIN_UPPERCASE_CHARS
+    MIN_PASSWORD_LENGTH, MIN_SPECIAL_CHARS, MIN_UPPERCASE_CHARS,    
+    MAX_TITLE_LENGTH, MAX_DESCRIPTION_LENGTH, MAX_TAG_LENGTH, 
+    MAX_MAIN_DESCRIPTION_LENTGH
 )
+
+class FormRestrictions:
+
+    def create_post(self) -> dict:
+        restrictions = {
+            'title_max_length': MAX_TITLE_LENGTH,
+            'main_description_max_length': MAX_MAIN_DESCRIPTION_LENTGH,
+            'description_max_length': MAX_DESCRIPTION_LENGTH,
+            'tag_max_length': MAX_TAG_LENGTH + 1 # account for comma at end of each tag
+        }
+        return restrictions
+    
+    def login_signup(self):
+        restrictions = {
+            'username_max_length': MAX_USERNAME_LENGTH,
+            'password_max_length': MAX_PASSWORD_LENGTH
+        }
+        return restrictions
+
+
+class Vote:
+
+    def vote_handler(self, request, form:Form, post_id:int, user_id:int, model:Model):
+        '''
+        Handle voting for votes on comments or posts.
+        - form = VoteOption / CommentVote
+        - post_id = id of post, used to redirect back to the current post 
+        - user_id = id of currently logged in user, for filtering models
+        - model = PostVote / CommentVote
+        '''
+        #can't vote if not logged in
+        if user_id == -1:
+            request.session['vote_error_msg'] = 'You must be logged in to vote'
+            request.session.modified = True
+            return redirect('post', post_id=post_id)
+        
+        #set kwargs_post_id (the pk for the model passed as the last parameter)
+        #set master_model (model to check if the user is voting on their own post)
+        if 'PostVote' in model.__name__:
+            kwargs_post_id = {'post_id': post_id}
+            master_model = Post
+        elif 'CommentVote' in model.__name__: 
+            kwargs_post_id = {'comment_id': post_id}
+            master_model = Comment
+
+        #check if the user votes on their own post
+        if len(master_model.objects.filter(user_id=user_id, **kwargs_post_id)) != 0:
+            request.session['vote_error_msg'] = "You can't vote on your own post"
+            request.session.modified = True
+            return redirect('post', post_id=post_id) 
+
+        current_vote = model.objects.filter(
+            user_id=user_id, **kwargs_post_id
+        )
+
+        option = form.cleaned_data.get('vote_option')
+        #deselect vote if same option selected as the previously selected option 
+        if option in current_vote.values_list('option', flat=True):
+            current_vote.delete()
+            return redirect('post', post_id=post_id)
+        
+        #delete previous vote
+        if len(current_vote) != 0:
+            current_vote.delete()
+
+        #save new vote
+        new_vote = model(
+            user_id=user_id,
+            option=option,   
+            **kwargs_post_id         
+        )
+        new_vote.save()
+    
 
 def is_image_file_extension_valid(image_path:str) -> bool:
     '''
@@ -10,7 +89,7 @@ def is_image_file_extension_valid(image_path:str) -> bool:
     Valid image extensions saved in config.py
     '''
     for extension in VALID_IMAGE_EXTENSIONS:
-        if image_path[-len(extension):] == extension:
+        if image_path[-len(extension):].lower() == extension:
             return True
     return False
 
@@ -19,7 +98,6 @@ def get_field_length_error(error:str) -> str:
     '''
     Return which field is has too many characters from form.errors
     '''
-
     if type(error) != str:
         error = str(error)
 
@@ -48,6 +126,7 @@ def is_password_strong_enough(password:str) -> bool:
         return True
     return False
 
+#POSSIBLY REDUNDANT
 
 def insert_spaces_in_tag_string(tag_string:str) -> str:
     '''
