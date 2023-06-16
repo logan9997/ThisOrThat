70b85403da4f2cae016f6b6aa5ea646d72bf17fb
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-06-16 18:29:13 +0100

    function for get posts, classes created to group query functionallity together

--- App/sql_queries.py
+++ App/sql_queries.py
@@ -1,7 +1,7 @@
 import psycopg2 
 from environment_manager import Manager
 
-class Query():
+class Query:
 
     def __init__(self) -> None:
         self.database_credentials = Manager().get_database_credentials('psycopg2')
@@ -16,3 +16,51 @@ class Query():
             return [row[0] for row in result]
         return result
     
+    def format(self, result:list, fields:list) -> list[dict]:
+        return [
+            {field: row[i] for i, field in enumerate(fields)} for row in result
+        ]
+
+    
+class Post(Query):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def get_posts(self, **kwargs):
+        '''
+        Return a list of all posts, 
+        ordered by : count of votes (DESC), date_posted (DESC)
+        '''        
+        posts = self.select(sql=f'''
+            SELECT PO.*, 
+            (
+                SELECT count(*) 
+                FROM "App_comment" CO 
+                WHERE CO.post_id = PO.post_id
+            ),
+            (
+                SELECT count(*) 
+                FROM "App_postvote" VO 
+                WHERE VO.post_id = PO.post_id
+            )
+            FROM "App_post" PO 
+            LEFT OUTER JOIN "App_postvote" VO ON PO.post_id=VO.post_id
+            {kwargs.get('where', '')}
+            GROUP BY PO.post_id
+            ORDER BY count(VO.*) DESC, date_posted DESC
+        '''
+        )
+
+        fields = [
+            'post_id', 'description_one', 'description_two', 'image_one', 
+            'image_two', 'status', 'date_posted', 'tags', 'user_id', 'title',
+            'main_description', 'comments', 'votes'
+        ]
+
+        print(kwargs.get('format', False), kwargs)
+        if kwargs.get('format', False):
+            posts = self.format(posts, fields)
+        print(posts)
+        return posts
+
