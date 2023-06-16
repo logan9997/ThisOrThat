651fb036ca29b4e13636fbeeaa02c2853de92bcb
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-06-16 18:32:28 +0100

    posts and comments for logged in user passed into context

--- App/views/home.py
+++ App/views/home.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from ..sql_queries import Post 
+from ..models import Comment
+from django.db.models import F
 
 def home(request):
 
+    user_id = request.session.get('user_id', -1)
+    posts = Post().get_posts(where=f'WHERE PO.user_id = {user_id}', format=True)
+    comments = Comment.objects.filter(user_id=user_id).values()#.annotate(post_id=F('post__post_id'))
+
     context = {
-        'title': 'Home'
+        'title': 'Home',
+        'posts': posts,
+        'comments': comments
     }
     return render(request, 'App/home.html', context=context)
