8cee46f809b32f646dd721b36e23b1465f1cb529
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-06-19 21:20:42 +0100

    get_comments method added, username added into select field in get_posts

--- App/sql_queries.py
+++ App/sql_queries.py
@@ -33,7 +33,7 @@ class Post(Query):
         ordered by : count of votes (DESC), date_posted (DESC)
         '''        
         posts = self.select(sql=f'''
-            SELECT PO.*, 
+            SELECT PO.*,
             (
                 SELECT count(*) 
                 FROM "App_comment" CO 
@@ -43,24 +43,62 @@ class Post(Query):
                 SELECT count(*) 
                 FROM "App_postvote" VO 
                 WHERE VO.post_id = PO.post_id
+            ),
+            (
+                SELECT username
+                FROM "App_user" US
+                WHERE US.user_id = PO.user_id
             )
-            FROM "App_post" PO 
+            FROM "App_post" PO
             LEFT OUTER JOIN "App_postvote" VO ON PO.post_id=VO.post_id
+            LEFT OUTER JOIN "App_comment" CO ON PO.post_id=CO.post_id
             {kwargs.get('where', '')}
             GROUP BY PO.post_id
-            ORDER BY count(VO.*) DESC, date_posted DESC
+            ORDER BY (COUNT(VO.*) + COUNT(CO.*)) DESC, date_posted DESC
         '''
         )
 
         fields = [
             'post_id', 'description_one', 'description_two', 'image_one', 
             'image_two', 'status', 'date_posted', 'tags', 'user_id', 'title',
-            'main_description', 'comments', 'votes'
+            'main_description', 'comments', 'votes', 'username'
         ]
 
-        print(kwargs.get('format', False), kwargs)
         if kwargs.get('format', False):
             posts = self.format(posts, fields)
-        print(posts)
         return posts
 
+    def get_comments(self, user_id, post_id, **kwargs):
+        comments = self.select(sql=f'''
+            SELECT comment, CO.comment_id, date_posted, US.user_id, username, (
+                SELECT option 
+                FROM "App_commentvote" CV 
+                WHERE user_id = {user_id}
+                    AND CV.comment_id = CO.comment_id
+            ) AS "option", (
+                (
+                    SELECT COUNT(*) 
+                    FROM "App_commentvote" CV 
+                    WHERE option='Up' 
+                        AND CV.comment_id = CO.comment_id
+                ) - (
+                    SELECT COUNT(*) 
+                    FROM "App_commentvote" CV 
+                    WHERE option='Down' 
+                    AND CV.comment_id = CO.comment_id
+                )
+            ) AS "votes"
+            FROM "App_comment" CO, "App_user" US
+            WHERE CO.user_id = US.user_id
+                AND post_id = {post_id}
+            ORDER BY votes DESC
+        ''')
+
+        fields = [
+            'comment', 'comment_id', 'date_posted', 'user_id', 
+            'username', 'current_vote', 'votes'
+        ]
+
+        if kwargs.get('format', False):
+            comments = self.format(comments, fields)
+        return comments
