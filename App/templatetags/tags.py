525cb730888cd3512e3f8f756237b6527ad0655b
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-06-21 23:38:06 +0100

    remove trailing withspace from tags

--- App/templatetags/tags.py
+++ App/templatetags/tags.py
@@ -42,6 +42,7 @@ def get_tags_list():
             tags = [tag_string]
 
         for tag in tags:
+            tag = tag.strip()
             if tag not in unique_tags:
                 unique_tags.append(tag)
     
