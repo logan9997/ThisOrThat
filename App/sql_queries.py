19fd392f35535ba3e544ba5ab30d2d222b65e8c3
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-06-21 23:41:26 +0100

    get_comments for home page works as intended

--- App/sql_queries.py
+++ App/sql_queries.py
@@ -8,6 +8,19 @@ class Query:
         self.conn = psycopg2.connect(**self.database_credentials)
         self.cursor = self.conn.cursor()
 
+    def format_result(func) -> list[dict]:
+        def inner(*args, **kwargs):
+            result = func(*args, **kwargs)
+
+            if kwargs.get('format', False) and kwargs.get('fields', False):
+                result = [
+                    {field: row[i] for i, field in enumerate(kwargs['fields'])} 
+                    for row in result
+                ]
+            return result
+        return inner
+
+    @format_result
     def select(self, sql, **kwargs):
         self.cursor.execute(sql)
         result = self.cursor.fetchall()
@@ -16,23 +29,20 @@ class Query:
             return [row[0] for row in result]
         return result
     
-    def format(self, result:list, fields:list) -> list[dict]:
-        return [
-            {field: row[i] for i, field in enumerate(fields)} for row in result
-        ]
-
     
 class Post(Query):
 
-    def __init__(self) -> None:
-        super().__init__()
+    def test(self):
+        sql = 'select * from "App_user"'
+        result = self.select(sql, format=True)
+        return result
 
     def get_posts(self, **kwargs):
         '''
         Return a list of all posts, 
         ordered by : count of votes (DESC), date_posted (DESC)
         '''        
-        posts = self.select(sql=f'''
+        sql=f'''
             SELECT PO.*,
             (
                 SELECT count(*) 
@@ -56,7 +66,6 @@ class Post(Query):
             GROUP BY PO.post_id
             ORDER BY (COUNT(VO.*) + COUNT(CO.*)) DESC, date_posted DESC
         '''
-        )
 
         fields = [
             'post_id', 'description_one', 'description_two', 'image_one', 
@@ -64,12 +73,10 @@ class Post(Query):
             'main_description', 'comments', 'votes', 'username'
         ]
 
-        if kwargs.get('format', False):
-            posts = self.format(posts, fields)
-        return posts
+        return self.select(sql, format=True, fields=fields)
 
-    def get_comments(self, user_id, post_id, **kwargs):
-        comments = self.select(sql=f'''
+    def get_comments(self, user_id, post_id):
+        sql=f'''
             SELECT comment, CO.comment_id, date_posted, US.user_id, username, (
                 SELECT option 
                 FROM "App_commentvote" CV 
@@ -92,13 +99,45 @@ class Post(Query):
             WHERE CO.user_id = US.user_id
                 AND post_id = {post_id}
             ORDER BY votes DESC
-        ''')
-
+        '''
         fields = [
             'comment', 'comment_id', 'date_posted', 'user_id', 
             'username', 'current_vote', 'votes'
         ]
 
-        if kwargs.get('format', False):
-            comments = self.format(comments, fields)
-        return comments
+        return self.select(sql, format=True, fields=fields)
+
+class Home(Query):
+    
+    def get_comments(self, user_id):
+        sql=f'''
+            SELECT CO.*, title, (
+                (
+                    SELECT COUNT(*) 
+                    FROM "App_commentvote" CV 
+                    WHERE option='Up' 
+                        AND CV.comment_id = CO.comment_id
+                ) - (
+                    SELECT COUNT(*) 
+                    FROM "App_commentvote" CV 
+                    WHERE option='Down' 
+                    AND CV.comment_id = CO.comment_id
+                )
+            ) AS "votes"
+            FROM "App_comment" CO, "App_user" US, "App_post" PO
+            WHERE CO.user_id = {user_id}
+                AND CO.post_id = PO.post_id
+                AND PO.user_id = US.user_id
+            ORDER BY votes DESC           
+        '''
+
+        fields = [
+            'comment_id', 'comment', 'date_posted', 'post_id', 
+            'user_id', 'post_title', 'votes'
+        ]
+
+        return self.select(sql, format=True, fields=fields)
+
+
+        
+        
