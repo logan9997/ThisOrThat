dcd9bfa541cbab7c047779578d0199378b194c89
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-06-25 18:57:18 +0100

    search panel and link to random post

--- App/views/home.py
+++ App/views/home.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from ..sql_queries import Post , Home
+from ..sql_queries import Post, Home
 from ..config import GetSorts
 from ..utils import Sort
+from ..models import Post as PostModel
+from random import choice
 
 def home(request):
 
@@ -21,6 +23,9 @@ def home(request):
     Sort.sort_dropdown_options(post_sorts, current_posts_sort)
     Sort.sort_dropdown_options(comment_sorts, current_comments_sort)
 
+    post_ids = PostModel.objects.all().values_list('post_id', flat=True)
+    random_post_id = choice(post_ids)
+
     context = {
         'title': 'Home',
         'posts': posts,
@@ -29,6 +34,7 @@ def home(request):
         'comment_sorts': comment_sorts,
         'current_posts_sort': current_posts_sort,
         'current_comments_sort': current_comments_sort,
+        'random_post_id': random_post_id
     }
     return render(request, 'App/home.html', context=context)
 
