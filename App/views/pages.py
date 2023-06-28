a850d2a72fe7c26fe4356b23b31b85d73da87dee
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-06-28 10:55:04 +0100

    set default of page = 1 and will created empty dict if get_params does not exist in session

--- App/views/pages.py
+++ App/views/pages.py
@@ -2,11 +2,15 @@ from django.shortcuts import redirect
 from ..forms import Page
 
 def pages(request):
+    page = 1
     if request.method == 'GET':
         form = Page(request.GET)
         if form.is_valid():
             page = form.cleaned_data.get('page')
     
+    if 'get_params' not in request.session:
+        request.session['get_params'] = {}
+
     #add query string to end of URL
     response = redirect('search')
     query_string = f'?page={page}'
